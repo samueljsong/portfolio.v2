8c6c1096c44115558759d89b717bdc99e59ec990
semual <semuall0108@gmail.com>
2024-05-11 17:22:29 -0700

    Finished landing page: Responsive with animations

--- public/js/landingpage.js
+++ public/js/landingpage.js
@@ -1,26 +1,68 @@
+
 document.addEventListener("DOMContentLoaded", () => {
+    const lines = document.querySelectorAll('.line');
+    const t1 = gsap.timeline();
+    const lineTimeline = gsap.timeline({ repeat: -1, repeatDelay: 0 });         //infinite repeat
+
     let windowSize = window.innerWidth;
     adjustHeadingBreak(windowSize);
+    setLineLength(lines);
     
     window.addEventListener('resize', () => {
         windowSize = window.innerWidth;
-        console.log(windowSize)
         adjustHeadingBreak(windowSize);
+        setLineLength(lines)
     })
 
-    const t1 = gsap.timeline()
 
-    const textArray = document.querySelectorAll('.landingpage > h1 > span');
+    t1.to('.landingpage > h1', {
+        opacity: 1,
+        duration: 0.3,
+        y: -20,
+    }, 1)
 
-    t1.from(textArray, {
-        y: 100,
-        opacity: 0,
-        duration: 0.5,
-        stagger: 0.15,
-    })
+    t1.to('nav', {
+        opacity: 1,
+        top: '2rem',
+        duration: 0.3
+    }, 1.3)
+
+    t1.to(lines, {
+        opacity: 1,
+        stagger: 0.08,
+        duration: 0.1,
+        ease: 'power2.in',
+        onComplete: () => {
+            lineTimeline.to(lines, {
+                opacity: 0,
+                stagger: 0.08,
+                duration: 0.4,
+                ease: 'power3.inOut'
+            }, 1)
+        
+            lineTimeline.to(lines, {
+                opacity: 1,
+                stagger: 0.08,
+                duration: 0.4,
+                ease: 'power3.inOut'
+            }, 1.4)
+        }
+    }, 1.15)
+
+    lineTimeline.delay(1)
+
     
-    
+
 })
+
+const setLineLength = (lines) => {
+    let multiplyVariable = 1;
+    let width = 100
+    lines.forEach((line) => {
+        line.style.width = `${width * multiplyVariable}%`
+        multiplyVariable *= 0.6
+    })
+}
 
 const adjustHeadingBreak = (windowSize) => {
     if(windowSize > 800){
@@ -32,12 +74,12 @@ const adjustHeadingBreak = (windowSize) => {
 
 const largeHeadingBreak = () => {
     let header = document.querySelector('.landingpage h1');
-    let newHtml = '<span>A</span> <span>passionate</span> <span>dev</span> <span>making</span><br><span class="gradient">things</span> <span>on</span> <span>the</span> <span>web.</span>';
+    let newHtml = 'A passionate dev making<br><span class="gradient">things</span> on the web.';
     header.innerHTML = newHtml;
 }
 
 const smallHeadingBreak = () => {
     let header = document.querySelector('.landingpage h1');
-    let newHtml = '<span>A</span> <span>passionate</span> <span>dev</span><br><span>making</span> <span class="gradient">things</span><br><span>on</span> <span>the</span> <span>web.</span>'
+    let newHtml = 'A passionate dev<br>making <span class="gradient">things</span><br>on the web.'
     header.innerHTML = newHtml;
 }
