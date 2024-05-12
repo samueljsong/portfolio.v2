f54934badc8636ae689124bf97dcc35e39f33624
samueljsong <semuall0108@gmail.com>
2024-05-12 00:08:34 -0700

    finished cursor animations for headers

---
+++ public/js/cursor.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    const headingText = document.querySelectorAll('h1');
+    const cursor = document.querySelector('.cursor');
+
+    headingText.forEach((h1) => {
+        h1.addEventListener('mouseenter', () => {
+            cursor.style.opacity = 1;
+            document.body.style.cursor = 'none';
+        })
+
+        h1.addEventListener('mouseleave', () => {
+            cursor.style.opacity = 0;
+            document.body.style.cursor = 'auto';
+        })
+    })
+
+    document.addEventListener('mousemove', (event) => {
+        let mouseX = event.clientX;
+        let mouseY = event.clientY;
+        
+        cursor.style.left = `${mouseX}px`
+        cursor.style.top = `${mouseY}px`
+    })
+})
