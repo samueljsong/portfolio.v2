6294bdfab525bbb90cae24579abd97f4e2a924a9
samueljsong <semuall0108@gmail.com>
2024-05-15 18:02:55 -0700

    new updated design of profile

--- public/js/aboutme.js
+++ public/js/aboutme.js
@@ -1,18 +1,34 @@
 document.addEventListener('DOMContentLoaded', () => {
 
+
     gsap.registerPlugin(ScrollTrigger);
 
-    let t1 = gsap.timeline();
-
-    t1.to('.aboutme > h1', {
-        opacity: 1,
-        y: -30,
+    let t1 = gsap.timeline({
         scrollTrigger : {
             trigger: '.aboutme',
-            start : 'top center',
+            start: 'top top',
             end: 'bottom bottom',
+            pin: '.aboutme-content',
             scrub: 1,
+            markers: false,
         }
+    });
+
+    t1.to('.aboutme-content > h1', {
+        y: -500,
+        opacity: 0,
+        duration:1,
+      })
+
+    t1.to('.techstack-text h1', {
+        opacity: 1,
+        y: -30,
+        stagger: 0.3,
+        duration: 1
     })
+
+    
+
+    
 
 })
