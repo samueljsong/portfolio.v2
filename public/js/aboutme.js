20b9a2c8ec3b3f896520b25bae59ca20751e9ccf
samueljsong <semuall0108@gmail.com>
2024-05-16 19:19:16 -0700

    changes in project

--- public/js/aboutme.js
+++ public/js/aboutme.js
@@ -1,34 +1,27 @@
-document.addEventListener('DOMContentLoaded', () => {
-
-
+document.addEventListener("DOMContentLoaded", () => {
     gsap.registerPlugin(ScrollTrigger);
 
     let t1 = gsap.timeline({
-        scrollTrigger : {
-            trigger: '.aboutme',
-            start: 'top top',
-            end: 'bottom bottom',
-            pin: '.aboutme-content',
+        scrollTrigger: {
+            trigger: ".aboutme",
+            start: "top top",
+            end: "bottom bottom",
+            pin: ".aboutme-content",
             scrub: 1,
             markers: false,
-        }
+        },
     });
 
-    t1.to('.aboutme-content > h1', {
+    t1.to(".aboutme-content > h1", {
         y: -500,
         opacity: 0,
-        duration:1,
-      })
+        duration: 1,
+    });
 
-    t1.to('.techstack-text h1', {
+    t1.to(".techstack-text h1", {
         opacity: 1,
         y: -30,
         stagger: 0.3,
-        duration: 1
-    })
-
-    
-
-    
-
-})
+        duration: 1,
+    });
+});
