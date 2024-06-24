125aca07d7162beb2529d3ae170fa8098cb8f716
samueljsong <semuall0108@gmail.com>
2024-06-23 20:59:35 -0700

    finished basic project section with animation and responsiveness

--- public/js/project.js
+++ public/js/project.js
@@ -1,65 +1,37 @@
 document.addEventListener("DOMContentLoaded", () => {
     gsap.registerPlugin(ScrollTrigger);
 
-    const t1 = gsap.timeline({
-        scrollTrigger: {
-            trigger: ".project",
-            start: "top top",
-            end: "bottom bottom",
-            pin: ".slider",
-            scrub: 1,
-            markers: false,
-        },
+    const projectCards = document.querySelectorAll(".project-card");
+
+    projectCards.forEach((project) => {
+        project.addEventListener("mouseover", () => {
+            const title = project.querySelector(".project-information > h3");
+
+            gsap.to(project, {
+                y: -10,
+                ease: "none",
+                duration: 0.1,
+            });
+
+            gsap.to(title, {
+                color: "#afbda1",
+                ease: "none",
+                duration: 0.1,
+            });
+        });
+
+        project.addEventListener("mouseleave", () => {
+            const title = project.querySelector(".project-information > h3");
+
+            gsap.to(project, {
+                y: 0,
+                duration: 0.1,
+            });
+
+            gsap.to(title, {
+                color: "#131313",
+                duration: 0.1,
+            });
+        });
     });
-    const project = document.querySelectorAll(".project-card");
-
-    console.log(project);
-
-    t1.from(".flaker-card", {
-        x: 300,
-        opacity: 0,
-    });
-
-    t1.to(".flaker-card", {
-        opacity: 1,
-    });
-
-    t1.to(".flaker-card", {
-        x: -300,
-        opacity: 0,
-    });
-
-    t1.from(".greenbanana-card", {
-        x: 300,
-        opacity: 0,
-    });
-
-    t1.to(".greenbanana-card", {
-        opacity: 1,
-    });
-
-    t1.to(".greenbanana-card", {
-        x: -300,
-        opacity: 0,
-    });
-
-    t1.from(".portfolio-card", {
-        x: 300,
-        opacity: 0,
-    });
-
-    t1.to(".portfolio-card", {
-        opacity: 1,
-    });
-
-    t1.to(".portfolio-card", {
-        x: -300,
-        opacity: 0,
-    });
-
-    // t1.to(project, {
-    //     x: 100,
-    //     opacity: 0,
-    //     stagger: 1,
-    // });
 });
